6575bacd2044e91d3445481f79ee6cfafcab1db6
yoon <kong4170@naver.com>
2023-09-12 22:33:50 +0900

    WeView, UIRepresentable

---
+++ SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorial/MyWebview.swift
@@ -0,0 +1,39 @@
+//
+//  MyWebview.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by 최윤제 on 2023/09/12.
+//
+
+import SwiftUI
+import WebKit
+
+// uikit의 uiView를 사용할 수 있도록 한다.
+// UIViewControllerRepresentable
+struct MyWebView: UIViewRepresentable {
+    
+    var urlToLoad: String
+    
+    // ui view 만들기
+    func makeUIView(context: Context) -> WKWebView {
+        
+        guard let url = URL(string: urlToLoad) else {return WKWebView()}
+        
+        let webview = WKWebView()
+        webview.load(URLRequest(url: url))
+        
+        return webview
+    }
+    
+    // 업데이트 ui view
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebView>) {
+         
+    }
+}
+
+
+struct MyWebView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyWebView(urlToLoad: "https://www.naver.com")
+    }
+}
---
+++ SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorial/SwiftUI_Webview_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Webview_tutorialApp.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by 최윤제 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Webview_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
