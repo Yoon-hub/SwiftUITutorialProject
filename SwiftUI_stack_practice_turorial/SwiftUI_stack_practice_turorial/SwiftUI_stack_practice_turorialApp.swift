9472801108889de540d27c62761e2d31e2f6fb70
yoon <kong4170@naver.com>
2023-09-21 12:50:38 +0900

    GeometryReader

--- SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorialApp.swift
+++ SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorialApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_stack_practice_turorialApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MyGeometryReader()
         }
     }
 }
