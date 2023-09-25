85c8899e1a2750dcbaeb843fffdec5affb195108
yoon <kong4170@naver.com>
2023-09-25 12:43:46 +0900

    TabBar

---
+++ SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorial/MyTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MyTabView.swift
+//  SwiftUI_stack_practice_turorial
+//
+//  Created by 최윤제 on 2023/09/24.
+//
+
+import SwiftUI
+
+struct MyTabView: View {
+    
+    var body: some View {
+        TabView {
+
+            MyView(text: "테스트1", color: .yellow)
+                .tabItem {
+                    Image(systemName: "airplane")
+                }
+                .tag(0)
+            MyView(text: "테스트2", color: .blue)
+                .tabItem {
+                    Image(systemName: "flame.fill")
+                }
+                .tag(1)
+            MyView(text: "테스트3", color: .green)
+                .tabItem {
+                    Image(systemName: "doc.fill")
+                }
+                .tag(2)
+        }
+        .onAppear() {
+            UITabBar.appearance().backgroundColor = .white
+        }
+        .toolbarBackground(.red, for: .tabBar)
+        
+        
+    
+    }
+}
+
+struct MyTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTabView()
+    }
+}
---
+++ SwiftUI_stack_practice_turorial/SwiftUI_stack_practice_turorial/MyView.swift
@@ -0,0 +1,35 @@
+//
+//  MyView.swift
+//  SwiftUI_stack_practice_turorial
+//
+//  Created by 최윤제 on 2023/09/25.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    
+    var text: String?
+    var color: Color?
+    
+    var body: some View {
+        ZStack {
+            color?
+                .edgesIgnoringSafeArea(.all)
+            Text(text!)
+                .font(.system(size: 40))
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+        
+        }
+        
+    }
+    
+}
+
+struct MyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyView(text: "1", color: .yellow)
+    }
+}
+
