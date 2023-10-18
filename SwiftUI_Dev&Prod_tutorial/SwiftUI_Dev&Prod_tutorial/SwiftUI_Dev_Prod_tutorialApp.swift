7613664a3ef42e7bb231f869421c1167c9799681
yoon <kong4170@naver.com>
2023-10-18 18:18:24 +0900

    Debug&Release

---
+++ SwiftUI_Dev&Prod_tutorial/SwiftUI_Dev&Prod_tutorial/SwiftUI_Dev_Prod_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Dev_Prod_tutorialApp.swift
+//  SwiftUI_Dev&Prod_tutorial
+//
+//  Created by 최윤제 on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Dev_Prod_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
